6530bc8112d3aea7592fb6ca605eca90cd0b949c
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-04-22 11:23:04 +0530

    feat: add NotFoundComponent with HTML, CSS, and test specifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ProjectComponent } from './project/project.component';
 import { TaskComponent } from './task/task.component';
 import { EditProjectComponent } from './edit-project/edit-project.component';
 import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -13,6 +14,5 @@ export const appRoutes: Routes = [
   { path: 'projects', component: ProjectComponent },
   { path: 'tasks/:projectTitle', component: TaskComponent },
   { path: 'projects/edit/:id', component: EditProjectComponent },
-
-  { path: '**', redirectTo: 'projects' },
+  { path: '**', component: NotFoundComponent },
 ];
